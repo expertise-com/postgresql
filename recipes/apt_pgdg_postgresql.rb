f9a89f802c7a3dea569d60414435eb33727a127c
Kyle Johnson <kyle@expertise.com>
2017-12-10 22:27:52 -0800

    Fix key ring

--- recipes/apt_pgdg_postgresql.rb
+++ recipes/apt_pgdg_postgresql.rb
@@ -9,10 +9,25 @@ file "remove deprecated Pitti PPA apt repository" do
   path "/etc/apt/sources.list.d/pitti-postgresql-ppa"
 end
 
+
+remote_file "#{Chef::Config[:file_cache_path]}/ACCC4CF8.asc" do
+  source 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
+  notifies :run, 'bash[apt-key-add]', :immediately
+end
+
+apt_update 'update'
+
+package 'apt-transport-https'
+
+bash 'apt-key-add' do
+  code "sudo apt-key add #{Chef::Config[:file_cache_path]}/ACCC4CF8.asc"
+  action :nothing
+end
+
 apt_repository 'apt.postgresql.org' do
   uri 'http://apt.postgresql.org/pub/repos/apt'
   distribution "#{node['postgresql']['pgdg']['release_apt_codename']}-pgdg"
   components ['main', node['postgresql']['version']]
-  key 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
+  # key 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
   action :add
 end
